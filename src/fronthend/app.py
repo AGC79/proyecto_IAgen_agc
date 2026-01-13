8e99deee2e7a7e924edd1a96aa02b751280cdf38
AGC79 <gudafreecam@gmail.com>
2026-01-13 09:20:07 +0100

    aplicacion actualizada

--- src/fronthend/app.py
+++ src/fronthend/app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(port=3000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000)
